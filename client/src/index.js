096d70ac235dc832d8a4df59dc1ab9eb4b120c1a
gabel35 <gabrieljose3135@gmail.com>
2021-04-10 14:40:39 -0400

    updated src folder

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './utils/css/App.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
