103e3ca03e7135607641473861df41e963460ecd
Jude <jantungkuno@gmail.com>
2021-04-15 16:33:31 -0400

    added images to be used in add post page

--- client/src/pages/AddPost/index.js
+++ client/src/pages/AddPost/index.js
@@ -2,3 +2,11 @@ import React from "react";
 import { Container, Row, Col, Jumbotron, Card, Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import "./style.css";
+import Header from '../../components/Header';
+import Footer from "../../components/Footer";
+import Customer from "../../assets/icons/customer.svg";
+import Football from "../../assets/icons/football.svg";
+import Dollar from "../../assets/icons/dollar-symbol.svg";
+import Birthday from "../../assets/icons/birthday-cake.svg";
+import Add from "../../assets/icons/add.svg";
+
