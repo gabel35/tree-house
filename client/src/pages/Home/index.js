fb7a6a8587c5b2c53775b1428fd04e89a1edec28
Jude <jantungkuno@gmail.com>
2021-04-15 17:28:14 -0400

    fixed routing error for logo image import in homepage index.js

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
-import logo from "./logo.png";
+import Logo from "../../assets/images/logo.png";
 
 function Home() {
   return (
