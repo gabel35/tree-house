1e2ffdfddcd83a0e4b1b4de4f18132774db2f0b5
Jude <jantungkuno@gmail.com>
2021-04-15 16:11:43 -0400

    added imports for Header.js file

---
+++ client/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import Logo from "../assets/images/logo.png";
+import "./Header.css"
+import Emails from "../assets/icons/envelope.svg";
+import User from "../assets/icons/User.svg";
+import LeftArrow from "../assets/icons/left-arrow.svg";
+
