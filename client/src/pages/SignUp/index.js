edcbf4e34ecb5171fd70d87c28565a267433a0e7
Jude <jantungkuno@gmail.com>
2021-04-15 16:57:43 -0400

    added imports for sign up index.js file

---
+++ client/src/pages/SignUp/index.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../Login/style.css";
+import SignUp from "../../components/SignUp";
+import '../Home/style.css';
+import Logo from "../../assets/images/logo.png";
+
