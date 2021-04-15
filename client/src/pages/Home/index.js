d3e898eb6a71baef4dd853c5db1525f2f5af844c
Jude <jantungkuno@gmail.com>
2021-04-15 16:48:29 -0400

    completed small edits to index.js for homepagae

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -10,20 +10,24 @@ function Home() {
       <div class="bg bg2"></div>
       <div class="bg bg3"></div>
       <div className="App">
+        <div class="content_box">
         <div>
-          <h1>WELCOME TO</h1>
-          <img src={logo} alt="Logo" />
-        </div>
-        <div>
-          <Link to="/login">
-            <button className="homepage-btn">log in</button>
-          </Link>
-          <Link to="/signup">
-            <button className="homepage-btn">sign up</button>
-          </Link>
-          <Link to="/forum">
-            <button className="homepage-btn">forum</button>
-          </Link>
+        <h1>WELCOME TO</h1>
+            <div className="logo">
+              <img src={Logo} alt="Logo" />
+            </div>
+          </div>
+          <div className="HomeButton">
+            <Link to="/login">
+              <button className="homepage-btn"><span>Log In</span></button>
+            </Link>
+            <Link to="/signup">
+              <button className="homepage-btn"><span>Sign Up</span></button>
+            </Link>
+            <Link to="/forum">
+              <button className="homepage-btn"><span>Forum</span></button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
