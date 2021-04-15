b036451cdefb8591632a156b66ee4b30b47b8c42
Jude <jantungkuno@gmail.com>
2021-04-15 16:22:37 -0400

    adding Login form box for sign up

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -2,4 +2,10 @@ import React, { useState} from 'react';
 import { Form } from 'react-bootstrap';
 import './LoginForm.css';
 
-function SignUp() {}
+function SignUp() {
+
+    return (
+        <Form className="LoginFormBox">
+            <div className="form-inner">
+                <h2>Sign Up</h2>
+                {/* {error!} */}
