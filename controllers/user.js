02a7a1f13b9f982d8091d28880aa96298866cab8
gabel35 <gabrieljose3135@gmail.com>
2021-04-17 19:22:02 -0400

    adjusted controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,32 @@
-const db = require("../models");
+const User = require("../models");
 
-// Defining methods for the booksController
+// Defining methods for the UsersController
 module.exports = {
   findAll: function(req, res) {
-    db.user
-      .find(req.query)
+    User.find(req.query)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  findById: function(req, res) {
+    User.findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    console.log("signup body", req.body)
+    User.create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    User.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    User.findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
 };
