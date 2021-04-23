e3559b6827508655968a022d1c551aa076feb864
Jacqueline Rodney <jacquelinerodney7@gmail.com>
2021-04-22 21:45:28 -0400

    clean up code server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const secureRoute = require('./routes/api/secure-routes');
+const secureRoute = require("./routes/api/secure-routes");
 const passport = require("passport");
 const session = require("express-session");
 const bodyParser = require("body-parser");
@@ -22,18 +22,18 @@ if (process.env.NODE_ENV === "production") {
 }
 
 //using backend routes
-app.use('/', routes);
+app.use("/", routes);
 
 // passport
 //secret session
-app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); 
+app.use(session({ secret: "keyboard cat",resave: true, saveUninitialized:true})); 
  
 app.use(passport.initialize());
  
 app.use(passport.session()); 
 
 // JWT strategy middleware
-app.use('/user', passport.authenticate("jwt", { session: false }), secureRoute);
+app.use("/user", passport.authenticate("jwt", { session: false }), secureRoute);
 
 // models
 console.log("models are:", models);
@@ -49,7 +49,7 @@ models.User.sync().then(function() {
 });
 
 //passport strategies
-require('./config/passport/passport.js')(passport, models.user);
+require("./config/passport/passport.js")(passport, models.user);
 
 
 //your app is being served
