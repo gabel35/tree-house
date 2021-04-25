54afb77455c777e0256c57e6208004d4f53a238f
Gabriel J. Sanchez <gabrieljose3135@gmail.com>
2021-04-24 22:28:40 -0400

    Update LoginForm.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -17,20 +17,19 @@ const submitHandler = e => {
                 <h2>Login</h2>
                 {/* {error!} */}
                 <Form.Group>
-                    <Form.Control type="text" name="name" id="name" onChange={e => setDetails({...details, name: e.target.value})} value={details.name} placeholder="Enter Name" />
-                </Form.Group>
-                <Form.Group>
                     <Form.Control type="email" name="email" id="email" onChange={e => setDetails({...details, email: e.target.value})} value={details.email} placeholder="Enter Email" />
                 </Form.Group>
                 <Form.Group>
                     <Form.Control type="password"  name="password" id="password" onChange={e => setDetails({...details, password: e.target.value})} value={details.password} placeholder="Enter Password" />
                 </Form.Group>
                 <Form.Group>
-                    <div className="homepage-btn">
-                        <span>
-                            <input type="submit" value="LOGIN" />
-                        </span>
-                    </div>
+                    <Link to="/profile">
+                        <div className="homepage-btn">
+                            <span>
+                                <input type="submit" value="LOGIN"/>
+                            </span>
+                        </div>
+                    </Link>
                 </Form.Group>
             </div>
         </Form>
