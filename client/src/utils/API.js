8c2c411fd114657e0474f93d6faaf8abf6b517b3
Gabriel J. Sanchez <gabrieljose3135@gmail.com>
2021-04-24 22:51:24 -0400

    Update API.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,11 +3,17 @@ import axios from "axios";
 export default {
   // Gets all users
   getUser: function() {
-    return axios.get("/api/user/login");
+    return axios.get("/api/user/profile");
   },
   // Saves a user to the database
   saveUser: function(userData) {
       console.log("frotend api user", userData)
     return axios.post("/api/user/signup", userData);
+  },
+    login: function(userData) {
+    console.log("frontend login", userData)
+    return axios.post("/api/user/login", userData);
   }
+  
+  
 };
